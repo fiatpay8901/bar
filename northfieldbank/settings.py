1b8c7d26320aa770a661e80f632e5b6c0cc5249d
fiatpay8901 <binancefiatpay@gmail.com>
2025-08-15 00:05:54 +0100

    database added

--- northfieldbank/settings.py
+++ northfieldbank/settings.py
@@ -77,15 +77,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'northfieldbank.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        default=os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=True,
+    )
 
 
 # Password validation
