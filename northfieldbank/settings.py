b732d4336b2193d99e1fcb071ec22709fadb3350
fiatpay8901 <binancefiatpay@gmail.com>
2025-08-14 22:54:00 +0100

    staticstorage

--- northfieldbank/settings.py
+++ northfieldbank/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
